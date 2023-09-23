c11e47dcd8edd0b533b05cb1b77879b5c8848d66
BilSayfer1 <142802154+BilSayfer1@users.noreply.github.com>
2023-09-23 14:58:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+let from = prompt('цена от')
+let up = prompt('цена до')
+
+let buy = priceList.filter((prise) => from <= from && up >= prise) 
+
+console.log(buy);
+
+
+let peoples = [`baxtzod`, `bobur`, `safir`, `abdumanon`, `paralellipiped `,`EnyetuenwuevueUgbemugbemOsas`, `shaxzod`, `islom`, `artyom`,`ismail`] 
+let names = peoples.filter((longest_name) => longest_name.length > peoples.length )
+console.log(names);
+
